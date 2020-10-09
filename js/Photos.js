dcbdfadcd3f04a19d281afff5687af940c3cdfee
Priyanka Farrell <priyanka@Nasrs-MacBook-Pro.local>
2020-10-09 11:34:18 -0600

    adding lat and long into routes

--- js/Photos.js
+++ js/Photos.js
@@ -1,7 +1,7 @@
 const Photos = () => {
 
     const grabAllPhotos = async (db, user_inspection_id) => {
-        return await db.any(`SELECT form_id,record_id,file_name FROM s3_photos WHERE record_id='${user_inspection_id}'`)
+        return await db.any(`SELECT form_id,record_id,file_name, exif_gps_latitude, exif_gps_longitude FROM s3_photos WHERE record_id='${user_inspection_id}'`)
     }
     
     const grabSinglePhoto = async (db, name) => {
